78049af777c12b4d441cc799a7d6b0a962935c4d
cod3burrito <robintanner44@gmail.com>
2022-10-06 18:13:19 -0400

    reworked functions

--- WonderREADME.md
+++ WonderREADME.md
@@ -1,2 +1,9 @@
+# Wonder JavaQuiz Extravaganza! 
+
+A quiz made with JavaScript all about (the basics) of JavaScript. All other quizzes cower in fear of it. Version 1, September 2022.
+
+## A section
+More details within the section, preferably a paragraph's worth.
+
 #### Sources
 Special thanks to Elad Shechter for the Reset CSS file.  https://elad2412.github.io/the-new-css-reset/
--- assets/script.js
+++ assets/script.js
@@ -1,57 +1,69 @@
-// function doQuiz() {
-//     go-btn-onclick() {
-//         // show quiz question one at a time
-//     }
-// };
-
-setInterval(quizTimer, 1000); //count down by 1000 milliseconds/1 second
-
-function quizTimer() {
-    // start timer and show first quiz question?
-    // no value initially; only gets value after quiz begins
-    // define length of timer (60 seconds)
-};
-
 var questions = [
     {
-        question: "True or False: JavaScript is related to Java.",
-        choiceA: "True",
-        choiceB: "False",
-        correct: "B: False"
-    }, {
-        question: "Commonly used data types DO NOT include:",
-        choiceA: "strings",
-        choiceB: "booleans",
-        choiceC: "alerts",
-        choiceD: "numbers",
-        correct: "C: alerts"
-    }, {
-        question: "The condition in an if/else statement is enclosed with __.",
-        choiceA: "quotes",
-        choiceB: "curly brackets",
-        choiceC: "parentheses",
-        choiceD: "square brackets",
-        correct: "C: Parentheses"
-    }, {
-        question: "Arrays in JavaScript can be used to store __.",
-        choiceA: "numbers and strings",
-        choiceB: "other arrays",
-        choiceC: "booleans",
-        choiceD: "all of the above",
-        correct: "D: All of the above"
-    }, {
-        question: "String values must be enclosed within __ when being assigned to variables.",
-        choiceA: "commas",
-        choiceB: "curly brackets",
-        choiceC: "quotes",
-        choiceD: "parentheses",
-        correct: "C: Quotes"
-    }, {
-        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        choiceA: "JavaScript",
-        choiceB: "terminal/bash",
-        choiceC: "for loops",
-        choiceD: "console.log",
-        correct: "D: console.log"
+    question: "True or False: JavaScript is related to Java.",
+            choiceA: "True",
+            choiceB: "False",
+            correct: "B: False"
+        }, {
+            question: "Commonly used data types DO NOT include:",
+            choiceA: "strings",
+            choiceB: "booleans",
+            choiceC: "alerts",
+            choiceD: "numbers",
+            correct: "C: alerts"
+        }, {
+            question: "The condition in an if/else statement is enclosed with __.",
+            choiceA: "quotes",
+            choiceB: "curly brackets",
+            choiceC: "parentheses",
+            choiceD: "square brackets",
+            correct: "C: Parentheses"
+        }, {
+            question: "Arrays in JavaScript can be used to store __.",
+            choiceA: "numbers and strings",
+            choiceB: "other arrays",
+            choiceC: "booleans",
+            choiceD: "all of the above",
+            correct: "D: All of the above"
+        }, {
+            question: "String values must be enclosed within __ when being assigned to variables.",
+            choiceA: "commas",
+            choiceB: "curly brackets",
+            choiceC: "quotes",
+            choiceD: "parentheses",
+            correct: "C: Quotes"
+        }, {
+            question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+            choiceA: "JavaScript",
+            choiceB: "terminal/bash",
+            choiceC: "for loops",
+            choiceD: "console.log",
+            correct: "D: console.log"
+        }
+    ];
+// } 
+
+//count down by 1000 milliseconds (1 second)
+function startQuiz(param) {
+    alert(param)
+    document.getElementById("quiz", "style: display").style.display;
     }
-];
+    timer = 10
+    let quizTimer = setInterval(() => {
+        console.log(timer--)
+        if(timer === 0){
+            alert('time is up!')
+            clearInterval(quizTimer)
+        }
+    }, 1000);
+    for(let i = 0; i < questions.length; i++) {
+        console.log(questions[i]);
+    };
+
+function goQuiz() {
+    // console.log("hello!");
+    // display timer and questions (1 at a time)
+    document.getElementById("timer", "style: block").style.display;
+    document.getElementById("quiz", "style: block");
+    questions.appendChild(quiz);
+}
