42d9afc46eca01806034bad4717cfc558e496c97
cod3burrito <robintanner44@gmail.com>
2022-10-12 08:13:51 -0400

    removed unnecessary files, added correct ones. repathed script srcs.

---
+++ assets/js/quiz.js
@@ -0,0 +1,75 @@
+var questions = [
+    {
+        question: "True or False: JavaScript is related to Java.",
+        choiceA: "True",
+        choiceB: "False",
+        correct: "B: False"
+    }, {
+        question: "Commonly used data types DO NOT include:",
+        choiceA: "strings",
+        choiceB: "booleans",
+        choiceC: "alerts",
+        choiceD: "numbers",
+        correct: "C: alerts"
+    }, {
+        question: "The condition in an if/else statement is enclosed with __.",
+        choiceA: "quotes",
+        choiceB: "curly brackets",
+        choiceC: "parentheses",
+        choiceD: "square brackets",
+        correct: "C: Parentheses"
+    }, {
+        question: "Arrays in JavaScript can be used to store __.",
+        choiceA: "numbers and strings",
+        choiceB: "other arrays",
+        choiceC: "booleans",
+        choiceD: "all of the above",
+        correct: "D: All of the above"
+    }, {
+        question: "String values must be enclosed within __ when being assigned to variables.",
+        choiceA: "commas",
+        choiceB: "curly brackets",
+        choiceC: "quotes",
+        choiceD: "parentheses",
+        correct: "C: Quotes"
+    }, {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choiceA: "JavaScript",
+        choiceB: "terminal/bash",
+        choiceC: "for loops",
+        choiceD: "console.log",
+        correct: "D: console.log"
+    }
+];
+
+var quizScore = 0;
+var quizletQuestions = 0;
+var timeRemaining = document.querySelector("#timer");
+var quiz = document.querySelector("#quiz");
+var quizBox = document.querySelector("#quizBox");
+
+var button = document.getElementById("go-btn"); 
+
+//timer elements
+function startQuiz(param) {
+    document.getElementById("quiz", "style: display").style.display;
+    }
+    timer = 75
+    let quizTimer = setInterval(() => {
+        console.log(timer--)
+        if(timer === 0){
+            alert("Time is up!")
+            clearInterval(quizTimer)
+        }
+    }, 1000);
+    for(let i = 0; i < questions.length; i++) {
+        console.log(questions[i]);
+    };
+
+button.addEventListener("click", function() {
+        // display timer and questions (1 at a time)
+        document.getElementById("timer", "style: block").style.display;
+        document.getElementById("quiz", "style: block");
+        questions.appendChild("quiz", "style: block");
+    }
+    );
